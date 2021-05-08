647fffe2aad9e1005f41efa435a07c8d70e4a0f9
Ashwin Dev <ashwindev1462001@gmail.com>
2021-05-08 20:39:14 +0530

    Clearing

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-
-
+#CSV file 1 for 50 data
 csv_file = open('coins.csv','w', newline='')
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(['name','symbol','URL'])
 
+#CSV file 2 for 1 Data
 csv_file2 = open('coins_data.csv','w', newline='')
 csv_writer2 = csv.writer(csv_file2)
 csv_writer2.writerow(['Symbol','Name','WatchlistCount','Website URL','Circulating Supply %','Price','Volume/Market Cap','Market Dominance',
@@ -23,6 +23,8 @@ def get_coins():
     count = 0
     cmc_links = soup.find_all('a',class_='cmc-link')
     selected_divs = soup.find_all('div',class_='sc-16r8icm-0 dnwuAU') 
+    
+    #For first 10 coins
     for div in selected_divs:
         if(count >= 50):
             break
@@ -33,6 +35,7 @@ def get_coins():
             csv_writer.writerow([name,symbol,URL])
             count += 1
 
+    #For remaining 40 coins
     for link in cmc_links:
         if(count >= 50):
             break
@@ -67,22 +70,18 @@ def get_coin_data(coin_symbol):
     soup = BeautifulSoup(source,'lxml')
     
     watch_list = soup.find_all('div',class_="namePill___3p_Ii")[2].text.split()[1]
-    
     circulating_supply = soup.find('div',class_="supplyBlockPercentage___1g1SF").text
       
     table = soup.find('div',class_='sc-16r8icm-0 fIhwvd')
     price = table.find('td').text
     
     volume_cap = table.find_all('tr')[4].td.text
-    
     market_dominance = table.find_all('tr')[5].td.span.text
-   
     rank = table.find_all('tr')[6].td.text
    
     
     table2 = soup.find_all('div',class_='sc-16r8icm-0 fIhwvd')[1]
     market_cap = table2.find('td').span.text
-    
     over_all_div = soup.find('div',class_='sc-1lt0cju-0 srvSa').div
     
     
@@ -114,16 +113,14 @@ def get_coin_data(coin_symbol):
     
     table3 = soup.find_all('div',class_="sc-16r8icm-0 fIhwvd")[3].tbody
     all_time_high_price = table3.find_all('tr')[4].td.span.text
-    
     all_time_low_price = table3.find_all('tr')[5].td.span.text
-    
     all_time_high_date = table3.find('small',class_='smallHeading___3DNdQ').text.split('(')[0]
-    
     all_time_low_date = table3.find_all('small',class_='smallHeading___3DNdQ')[1].text.split('(')[0]
     
-    
+    #Writing the data to csv file 2
     csv_writer2.writerow([coin_symbol,name,watch_list,website_url,circulating_supply,price,volume_cap,market_dominance,rank,market_cap,all_time_high_date,all_time_high_price,all_time_low_date,all_time_low_price,what_is_coin_name,who_are_the_founders,what_makes_it_unique])
     csv_file2.close()
-    
+   
+#Function call 
 get_coins()
 get_coin_data('AVAX')
