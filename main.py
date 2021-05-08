eeb041551ef2feff40444277554daad921d507ec
Ashwin Dev <ashwindev1462001@gmail.com>
2021-05-08 18:58:01 +0530

    Cleaned

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
-
-
-import bs4 as bs
-import sys
-import urllib.request
-from PyQt5.QtWebEngineWidgets import QWebEnginePage
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QUrl
-
-class Page(QWebEnginePage):
-    def __init__(self, url):
-        self.app = QApplication(sys.argv)
-        QWebEnginePage.__init__(self)
-        self.html = ''
-        self.loadFinished.connect(self._on_load_finished)
-        self.load(QUrl(url))
-        self.app.exec_()
-
-    def _on_load_finished(self):
-        self.html = self.toHtml(self.Callable)
-        print('Load finished')
-
-    def Callable(self, html_str):
-        self.html = html_str
-        self.app.quit()
+from bs4 import BeautifulSoup
+import requests
+import csv
 
 url = "https://coinmarketcap.com/"
-def main():
-    page = Page(url)
-    soup = bs.BeautifulSoup(page.html, 'html.parser')
-    
+source = requests.get(url).text
+soup = BeautifulSoup(source,'lxml')
+
+csv_file = open('coins.csv','w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['name','symbol','URL'])
+
+def get_coins():
+    count = 0
     cmc_links = soup.find_all('a',class_='cmc-link')
     selected_divs = soup.find_all('div',class_='sc-16r8icm-0 dnwuAU') 
     for div in selected_divs:
-        name = div.find('p',class_='sc-1eb5slv-0 iJjGCS').text
-        symbol = div.find('p',class_='sc-1eb5slv-0 gGIpIK coin-item-symbol').text
-        URL = "coinmarketcap.com" + div.find('a',class_='cmc-link')['href']
-        print(name,symbol,URL)
-    for link in cmc_links:
-        try:
-            name = link.find('span',class_="").text
-            symbol = link.find('span',class_='crypto-symbol').text
-            URL = "coinmarketcap.com" + link['href']
-            print(name,symbol,URL)
-        except:
-            pass
-    
+        if(count > 50):
+            break
+        else:
+            name = div.find('p',class_='sc-1eb5slv-0 iJjGCS').text
+            symbol = div.find('p',class_='sc-1eb5slv-0 gGIpIK coin-item-symbol').text
+            URL = "coinmarketcap.com" + div.find('a',class_='cmc-link')['href']
+            csv_writer.writerow([name,symbol,URL])
+            count += 1
 
-if __name__ == '__main__': main()
+    for link in cmc_links:
+        if(count > 50):
+            break
+        else:
+            try:
+                name = link.find('span',class_="").text
+                symbol = link.find('span',class_='crypto-symbol').text
+                URL = "coinmarketcap.com" + link['href']
+                csv_writer.writerow([name,symbol,URL])
+                count += 1
+            except:
+                pass
+    csv_file.close()
+get_coins()
--- phantomjs.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-driver = webdriver.PhantomJS()
-#driver = webdriver.PhantomJS()
-driver.get("https://ypa.herokuapp.com/")
-p_element = driver.find_element_by_id(id_='target')
-print(p_element.text)
